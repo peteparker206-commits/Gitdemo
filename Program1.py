fc12a520b0fc1117b035dc08d8f23d83821a4126
peteparker206-commits <peteparker206@gmail.com>
2025-12-27 15:05:27 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,14 @@
+num_1 = int(input("Enter your number1 "))
+num_2 = int(input("Enter your number2 "))
+
+add = num_1 + num_2
+sub = num_1 - num_2
+mul = num_1 * num_2
+div = num_1 / num_2
+
+print("The values entered are: ", num_1 , "and" , num_2)
+
+print("Additiojn value: ", add)
+print("Subtraction value: ", sub)
+print("Multiplicaton value: ", mul)
+print("Division value: ", div)
