d3097e8b9bfcb70fca02cdb3fd95bf55856c5d56
peteparker206-commits <peteparker206@gmail.com>
2026-01-01 20:02:37 +0530

    Update Program2.py

--- Program2.py
+++ Program2.py
@@ -1,4 +1,13 @@
-first_name= input("Enter your first name: ")
-last_name= input("Enter your last name: ")
-name = first_name + last_name
-print("Hello,", name,"!","Welcome to the Python program" )
+with open("sample.txt", "at") as f:
+    
+    add1= input("Enter text to write in the file: \n")
+    f.write(add1)
+
+    add2= input("Enter additional information to write in the file: \n")
+    f.write(add2)
+
+with open("sample.txt", "rt") as fh:
+    
+    a = fh.read()
+    print(a)
+ 
